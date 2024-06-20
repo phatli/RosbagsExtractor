9f2ee50c4e24294e415d0bf8a73ea36534aae7dc
Li <heshan.li@ntu.edu.sg>
2024-06-20 06:24:11 +0000

    feat: add topic selection flexibility

--- ExtractRosbags.py
+++ ExtractRosbags.py
@@ -5,59 +5,115 @@ import math
 import numpy as np
 import pandas as pd
 import argparse
-from os.path import join, isfile, exists
+from os.path import join, exists
 from os import makedirs
 import glob
 from tqdm import tqdm
 import cv2
 
 
+def find_topics(topics, data_types):
+    """Find topics that could potentially contain data of the given types."""
+    possible_topics = []
+    for data_type in data_types:
+        possible_topics.extend(
+            [topic for topic, (topic_type, _, _, _) in topics.items() if data_type in topic_type])
+    return possible_topics
 
-def extract_gps_msg(gps_msgs, timestamp):
-    """Extract the closest NavSatFix message to a given timestamp."""
+
+def user_select_topic(possible_topics, data_description):
+    """Prompt user to select a topic from a list of possible topics."""
+    if not possible_topics:
+        raise ValueError(
+            f"No topics found for {data_description}. This data type is essential.")
+    print(f"Select a topic for {data_description}:")
+    possible_topics.append(None)
+    for idx, topic in enumerate(possible_topics):
+        print(f"{idx + 1}: {topic}")
+    selection = int(input("Enter the number of your choice: ")) - 1
+    return possible_topics[selection]
+
+
+def get_topics_to_read(selected_topics, bag):
+    """
+    Validate and select topics for data extraction.
+    If topics in selected_topics are not valid for the current bag file, user is prompted to select new topics.
+
+    Args:
+    selected_topics (dict): A dictionary with keys 'points_topic', 'gps_topic', 'image_topic' and their current values.
+    bag (rosbag.Bag): The currently opened rosbag file.
+
+    """
+    topics = bag.get_type_and_topic_info()[1]
+    current_topics = topics.keys()
+    topics_to_read = []
+
+    # GPS topic validation and selection
+    if selected_topics['gps']['topic'] is None or selected_topics['gps']['topic'] not in current_topics:
+        possible_gps_topics = find_topics(topics, ['sensor_msgs/NavSatFix'])
+        assert possible_gps_topics, "Can't find any GPS topic."
+        selected_topics['gps']['topic'] = user_select_topic(
+            possible_gps_topics, 'GPS') if len(possible_gps_topics) > 1 else possible_gps_topics[0]
+    topics_to_read.append(selected_topics['gps']['topic'])
+
+    # Points topic validation and selection
+    if selected_topics['points']['topic'] is None or selected_topics['points']['topic'] not in current_topics:
+        possible_points_topics = find_topics(
+            topics, ['sensor_msgs/PointCloud2', 'msgs_radar/RadarScanExtended'])
+        selected_topics['points']['topic'] = user_select_topic(
+            possible_points_topics, 'Points') if possible_points_topics else None
+    if selected_topics['points']['topic']:
+        topics_to_read.append(selected_topics['points']['topic'])
+
+    # Image topic validation and selection
+    if selected_topics['image']['topic'] is None or selected_topics['image']['topic'] not in current_topics:
+        possible_image_topics = find_topics(
+            topics, ['sensor_msgs/Image', 'sensor_msgs/CompressedImage'])
+        selected_topics['image']['topic'] = user_select_topic(
+            possible_image_topics, 'Image') if possible_image_topics else None
+    selected_topics['image']['is_compressed'] = selected_topics['image']['topic'] in find_topics(topics, ['sensor_msgs/CompressedImage'])
+    if selected_topics['image']['topic']:
+        topics_to_read.append(selected_topics['image']['topic'])
+
+    return selected_topics, topics_to_read
+
+
+def extract_msg(msgs, timestamp):
     # Use binary search to find the index of the closest gps message
-    idx = np.searchsorted(gps_msgs['timestamps'], timestamp)
+    idx = np.searchsorted(msgs['timestamps'], timestamp)
     if idx == 0:
-        return gps_msgs['data'][0]
-    elif idx == len(gps_msgs['data']):
-        return gps_msgs['data'][-1]
+        return msgs['data'][0]
+    elif idx == len(msgs['data']):
+        return msgs['data'][-1]
     else:
         # Choose the closest of the two neighboring messages
-        prev_delta = abs(gps_msgs['timestamps'][idx-1] - timestamp)
-        next_delta = abs(gps_msgs['timestamps'][idx] - timestamp)
+        prev_delta = abs(msgs['timestamps'][idx-1] - timestamp)
+        next_delta = abs(msgs['timestamps'][idx] - timestamp)
         if prev_delta < next_delta:
-            return gps_msgs['data'][idx-1]
+            return msgs['data'][idx-1]
         else:
-            return gps_msgs['data'][idx]
+            return msgs['data'][idx]
 
-def extract_img_msg(img_msgs, timestamp):
-    idx = np.searchsorted(img_msgs['timestamps'], timestamp)
-    if idx == 0:
-        return img_msgs['data'][0]
-    elif idx == len(img_msgs['data']):
-        return img_msgs['data'][-1]
-    else:
-        prev_delta = abs(img_msgs['timestamps'][idx-1] - timestamp)
-        next_delta = abs(img_msgs['timestamps'][idx] - timestamp)
-        if prev_delta < next_delta:
-            return img_msgs['data'][idx-1]
-        else:
-            return img_msgs['data'][idx]
 
 def save_image(image_msg, frame_time, save_root, rosbag_name, is_compressed):
     """Save the image data to a file."""
     image_path = join(save_root, rosbag_name, "images")
     if not exists(image_path):
         makedirs(image_path)
-    
+
     if is_compressed:
         np_arr = np.frombuffer(image_msg.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     else:  # Assume raw image
-        image_np = np.frombuffer(image_msg.data, dtype=np.uint8).reshape(image_msg.height, image_msg.width, -1)
+        image_np = np.frombuffer(image_msg.data, dtype=np.uint8).reshape(
+            image_msg.height, image_msg.width, -1)
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
 
-    image_filename = f'{frame_time}.png'  # You can choose other formats like jpg
+
+    # You can choose other formats like jpg
+    image_filename = f'{frame_time}.png'
     cv2.imwrite(join(image_path, image_filename), image_np)
+
 
 def save_data_to_files(data, bag_timestamp, save_root, rosbag_name):
     """Save the extracted data to the files."""
@@ -72,9 +128,9 @@ def save_data_to_files(data, bag_timestamp, save_root, rosbag_name):
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--points_topic', default="/radar_enhanced_pcl",
+    parser.add_argument('--points_topic', default=None,
                         help='The topic containing the point cloud data')
-    parser.add_argument('--gps_topic', default="/ublox/fix",
+    parser.add_argument('--gps_topic', default=None,
                         help='The topic containing the GPS data')
     parser.add_argument('--image_topic', default=None,
                         help='The topic containing the image data')
@@ -82,17 +138,33 @@ if __name__ == '__main__':
                         help='The list of bag files to process')
     parser.add_argument('--save_folder', default="ros_datasets/extracted_data",
                         help='Root folder to save extracted data')
+    parser.add_argument('--fps', type=float, default=4, help='Sampling interval for GPS data in seconds')
+
+
     args = parser.parse_args()
 
-    is_polar_coord = False
-    if args.points_topic == "/radar_scan" and args.gps_topic == "/gps/fix":
-        is_polar_coord = True
+
+    selected_topics = {
+        'points':{
+            'topic': args.points_topic,
+            'is_polar': False
+            },
+        'gps': {
+            'topic': args.gps_topic
+        },
+        'image': 
+        {
+            'topic': args.image_topic,
+            'is_compressed': False
+        }
+    }
 
     # List of bag files to process
     bag_filenames = sorted(
         [file_name for pattern in args.bags for file_name in glob.glob(
             pattern)]
     )
+    ds_dict = {}
 
     for bag_filename in bag_filenames:
         filename = bag_filename.split("/")[-1]
@@ -102,108 +174,110 @@ if __name__ == '__main__':
             rosbag_name = f"{date}_{rosbag_name}"
         else:
             rosbag_name = filename.split(".")[0]
-        print(f'Processing { bag_filename}...')
+        if rosbag_name not in ds_dict:
+            ds_dict[rosbag_name] = [bag_filename]
+        else:
+            ds_dict[rosbag_name].append(bag_filename)
 
-        # Lists to store the extracted data
-        pcl_data = []
+    for rosbag_name, bags_filename in ds_dict.items():
+        points_data = []
         gps_data = []
-        topics_to_read = [args.points_topic, args.gps_topic]
-        if args.image_topic:
-            topics_to_read.append(args.image_topic)
-            img_timestamps = []
-            is_compressed = None
-        # Open the bag file and extract the messages
-        bag = rosbag.Bag(bag_filename, "r")
-        total_messages = bag.get_message_count(topic_filters=topics_to_read)
-        with tqdm(total=total_messages, desc=f"Extracting from {bag_filename}", unit="msg") as pbar:
-            for topic, msg, t in bag.read_messages(topics=topics_to_read):
-                if topic == args.points_topic:
-                    if is_polar_coord:
-                        points = np.array([[target.range * math.cos(target.elevation) * math.cos(target.azimuth),
-                                            target.range *
-                                            math.cos(target.elevation) *
-                                            math.sin(target.azimuth),
-                                            target.range * math.sin(target.elevation)]
-                                        for target in msg.targets])
-                        speed = np.array(
-                            [target.velocity for target in msg.targets])
-                        power = np.array([target.power for target in msg.targets])
-                    else:
-                        points = np.array([[point.x, point.y, point.z]
-                                        for point in msg.points])
-                        speed = np.array(msg.channels[0].values)
-                        power = np.array(msg.channels[2].values)
-
-                    pcl_data.append((t.to_sec(), np.hstack(
-                        (points, speed.reshape(-1, 1), power.reshape(-1, 1)))))
-
-                elif topic == args.gps_topic:
-                    gps_data.append(
-                        (t.to_sec(), np.array([msg.latitude, msg.longitude, msg.altitude])))
-                elif topic == args.image_topic:
-                    img_timestamps.append((t.to_sec(), msg))
-                    if is_compressed is None:
-                        is_compressed = msg._type == "sensor_msgs/CompressedImage"
-                pbar.update(1)
+        image_data = []
+        for indx, bag_filename in enumerate(bags_filename, 1):
+            print(f'Processing {rosbag_name}: {indx}/{len(bags_filename)}')
 
 
-        bag.close()
+            bag = rosbag.Bag(bag_filename, "r")
 
-        assert len(pcl_data) !=0 and len(gps_data) != 0, "No data extracted from the bag file."
-        if args.image_topic:
-            assert is_compressed is not None, "Cannot find image topic in rosbag."
+            if indx == 1:
+                selected_topics, topics_to_read = get_topics_to_read(
+                    selected_topics, bag)
+
+                if selected_topics['points']['topic'] == "/radar_scan" and selected_topics['gps']['topic'] == "/gps/fix":
+                    selected_topics["points"]["is_polar"] = True
+
+            total_messages = bag.get_message_count(
+                topic_filters=topics_to_read)
+
+
+            with tqdm(total=total_messages, desc=f"Extracting from {bag_filename}", unit="msg") as pbar:
+                for topic, msg, t in bag.read_messages(topics=topics_to_read):
+                    if topic == selected_topics['points']['topic']:
+                        if selected_topics['points']['is_polar']:
+                            points = np.array([[target.range * math.cos(target.elevation) * math.cos(target.azimuth),
+                                                target.range *
+                                                math.cos(target.elevation) *
+                                                math.sin(target.azimuth),
+                                                target.range * math.sin(target.elevation)]
+                                               for target in msg.targets])
+                            speed = np.array(
+                                [target.velocity for target in msg.targets])
+                            power = np.array(
+                                [target.power for target in msg.targets])
+                        else:
+                            points = np.array([[point.x, point.y, point.z]
+                                               for point in msg.points])
+                            speed = np.array(msg.channels[0].values)
+                            power = np.array(msg.channels[2].values)
+
+                        points_data.append((t.to_sec(), np.hstack(
+                            (points, speed.reshape(-1, 1), power.reshape(-1, 1)))))
+
+                    elif topic == selected_topics['gps']['topic']:
+                        gps_data.append(
+                            (t.to_sec(), np.array([msg.latitude, msg.longitude, msg.altitude])))
+                    elif topic == selected_topics['image']['topic']:
+                        image_data.append((t.to_sec(), msg))
+                    pbar.update(1)
+
+            bag.close()
+
+
+        assert len(gps_data) != 0, "No GPS data extracted."
 
         # Convert lists to numpy arrays for faster processing
         pcl_data = np.array(
-            pcl_data, dtype=[('timestamps', float), ('data', object)], ndmin=1)
+            points_data, dtype=[('timestamps', float), ('data', object)], ndmin=1)
         gps_data = np.array(
             gps_data, dtype=[('timestamps', float), ('data', object)], ndmin=1)
-        if args.image_topic:
-            img_data = np.array(
-                img_timestamps, dtype=[('timestamps', float), ('data', object)], ndmin=1
-            )
+        img_data = np.array(
+            image_data, dtype=[('timestamps', float), ('data', object)], ndmin=1
+        )
+
+        gps_data = np.sort(gps_data, order='timestamps')
 
         # Sort the arrays by timestamp for faster searching
         pcl_data = np.sort(pcl_data, order='timestamps')
-        gps_data = np.sort(gps_data, order='timestamps')
-        if args.image_topic:
-            img_data = np.sort(img_data, order='timestamps')
+        img_data = np.sort(img_data, order='timestamps')
 
 
         csv_filename = join(args.save_folder, rosbag_name,
                             f"{rosbag_name}_gps.csv")
-        if isfile(csv_filename):
-            existing_df = pd.read_csv(csv_filename)
-        else:
-            existing_df = pd.DataFrame(
-            columns=['timestamp', 'latitude', 'longitude', 'altitude'])
-        
 
         gps_df = pd.DataFrame(
             columns=['timestamp', 'latitude', 'longitude', 'altitude'])
 
+        interval = 1.0 / args.fps
+        last_processed_time = None
         # Extract the data from each PCL msg and save it to a numpy array
-        for timestamp, pcl_msg in tqdm(pcl_data, desc=f"==> Writing data", leave=True, position=0):
-            gps_info = extract_gps_msg(gps_data, timestamp)
-            if args.image_topic:
-                img_msg = extract_img_msg(img_data, timestamp)
-                save_image(img_msg, "{:.6f}".format(timestamp).replace(".", ""), args.save_folder, rosbag_name, is_compressed)
+        for timestamp, gps_info in tqdm(gps_data, desc=f"==> Writing data", leave=True, position=0):
+            if last_processed_time is not None and (timestamp - last_processed_time) < interval:
+                continue
+            last_processed_time = timestamp
+            if len(pcl_data) > 0:
+                pcl_msg = extract_msg(pcl_data, timestamp)
+                save_data_to_files(
+                    pcl_msg, f'{timestamp}', args.save_folder, rosbag_name)
+            if len(img_data) > 0:
+                img_msg = extract_msg(img_data, timestamp)
+                save_image(img_msg, "{:.6f}".format(timestamp).replace(
+                    ".", ""), args.save_folder, rosbag_name, selected_topics["image"]["is_compressed"])
             timestamp = "{:.6f}".format(timestamp).replace(".", "")
             new_row = {"timestamp": timestamp,
                        "latitude": gps_info[0], "longitude": gps_info[1], "altitude": gps_info[2]}
 
             gps_df.loc[len(gps_df)] = new_row
 
-            save_data_to_files(
-                pcl_msg, f'{timestamp}', args.save_folder, rosbag_name)
-
-        # Only keep rows with timestamps not in the existing DataFrame
-        new_rows = gps_df[~gps_df['timestamp'].astype(int).isin(existing_df['timestamp'].astype(int))]
-
-        # Concatenate new rows with the existing DataFrame
-        updated_df = pd.concat([existing_df, new_rows], ignore_index=True)
-
 
         # Write updated DataFrame to CSV
-        updated_df.to_csv(csv_filename, header=True, index=False)
-        
+        gps_df.to_csv(csv_filename, header=True, index=False)
--- README.md
+++ README.md
@@ -1,21 +1,64 @@
-# Rosbag PCL Extractor
+# ROS Bag Data Extractor
 
-This script is used to extract point cloud data from ROSBAG files and save them as numpy arrays. It also extracts GPS data and saves it as a CSV file, with each row containing the timestamp, latitude, longitude, and altitude.
+- [ROS Bag Data Extractor](#ros-bag-data-extractor)
+  - [Features](#features)
+  - [Requirements](#requirements)
+  - [Setup](#setup)
+  - [Usage](#usage)
+  - [Example](#example)
+
+
+This Python script is designed to process ROS bag files to extract GPS, point cloud, and image data, handling various topic types and allowing dynamic topic selection. It features an interactive mode to choose topics if not specified, and it can handle both compressed and uncompressed image data.
+
+## Features
+
+- **Dynamic Topic Selection**: If the topics are not specified or are not present in the bag, the script will prompt the user to choose from available topics.
+- **Data Extraction**: Extracts and processes GPS data, point cloud data, and images from specified topics within ROS bag files.
+- **Data Saving**: Extracted data is saved to specified output directories, organized by dataset names derived from file names.
+- **Compression Handling**: Determines if image data is compressed and processes it accordingly.
+
+## Requirements
+
+- Python 3
+- ROS environment
+- OpenCV (cv2), NumPy (numpy), Pandas (pandas), and tqdm libraries
+
+## Setup
+
+1. **Install Dependencies**: Ensure that you have ROS and the required Python packages installed. You can install the necessary Python packages using pip:
+   pip install numpy pandas opencv-python tqdm
+
+2. **Prepare ROS Environment**: This script assumes that you have a ROS environment set up and that you are familiar with the basics of ROS topics and bag files.
 
 ## Usage
 
-`python rosbag_pcl_extractor.py --points_topic <PCL_TOPIC> --gps_topic <GPS_TOPIC> --image_topic <IMG_TOPIC> --bags <BAG_FILENAME1> <BAG_FILENAME2> ... --save_folder <SAVE_FOLDER>`
+1. **Prepare Your Bag Files**: Place your .bag files in a directory or specify the path to each bag file directly.
 
-#### Arguments:
+2. **Specify Topics (Optional)**: If known, you can specify the topics for GPS, point cloud, and image data via command-line arguments. If not specified, the script will allow you to select from available topics in each bag file.
 
-- `--points_topic`: The ROS topic containing the point cloud data (default: `/radar_enhanced_pcl`).
-- `--gps_topic`: The ROS topic containing the GPS data (default: `/ublox/fix`).
-- `--image_topic`: The ROS topic containing the image data (default: None)
-- `--bags`: A list of ROSBAG files to process.
-- `--save_folder`: The root folder to save the extracted data (default: `../extracted_data`).
+3. **Run the Script**:
+   Use the following command to run the script, replacing <your_bag_files> with your bag file paths:
 
-## Output
+   ```shell
+   python3 extract_ros_data.py --bags <your_bag_files> --save_folder <output_directory>
+   ```
 
-- Point cloud data is saved as numpy arrays in the `points` subfolder of the `SAVE_FOLDER`, with the filename as the timestamp of the message.
-- Image data is saved as `.png` in the `images` subfolder of the `SAVE_FOLDER`, with the filename as the timestamp of the message.
-- GPS data is saved as a CSV file in the `SAVE_FOLDER`, with the filename as `<ROSBAG_NAME>_gps.csv`. If the file already exists, new data is appended to it.
+   Here are the available arguments:
+
+   - --points_topic: Topic for point cloud data.
+   - --gps_topic: Topic for GPS data.
+   - --image_topic: Topic for image data.
+   - --bags: List of bag files to process (required).
+   - --save_folder: Root folder to save extracted data (default: ros_datasets/extracted_data).
+   - --fps: Frame rate to sample the GPS data (default: 4 frames per second).
+
+## Example
+
+To process files located in /path/to/bags and save the extracted data in /path/to/output, run:
+
+```shell
+python3 extract_ros_data.py --bags /path/to/bags/*.bag --save_folder /path/to/output
+```
+
+If topics are not specified, the script will interactively prompt you to choose topics from each ROS bag file. This process is repeated for each unique dataset identified by the bag filenames.
+
